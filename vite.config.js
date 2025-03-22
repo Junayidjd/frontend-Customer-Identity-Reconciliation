67f4df92286f3b062c2329ac0651ff62ab745ede
junaid <junaidjdsiddiqui21@gmail.com>
2025-03-22 12:18:23 +0530

    commmit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     proxy: {
-//       "/identify": {
-//         // target: 'http://localhost:3000', // Backend server URL
-//         target: "https://backend-assignment-moonrider.onrender.com", // Backend server URL
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
-
-
-
-
-
-
-
-
-
-
-
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
